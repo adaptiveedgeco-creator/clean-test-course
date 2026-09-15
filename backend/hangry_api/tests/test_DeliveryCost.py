d0a6afc919bd8d68f9dab7ace66cb0ac7137e31f
adaptiveedgeco-creator <adaptiveedgeco@gmail.com>
2026-09-15 11:51:07 -0400

    Update default delivery fee unit test

--- backend/hangry_api/tests/test_DeliveryCost.py
+++ backend/hangry_api/tests/test_DeliveryCost.py
@@ -1,37 +1,28 @@
 from api.controllers import Delivery
-from django_mock_queries.query import MockSet, MockModel
+from django_mock_queries.query import MockModel, MockSet
 
-def test_LotsOfItems():
-  #Arrange
-  order = MockSet()
-  order.add(MockModel(quantity=5))
-  order.add(MockModel(quantity=5))
-  order.add(MockModel(quantity=5))
-  delivery_distance = 6
-  #Act
-  cost = Delivery.calculate(order,delivery_distance)
-  #Assert
-  assert cost == 7.5
 
-def test_MiddleOfTheRoadItems():
-  #Arrange
-  order = MockSet()
-  order.add(MockModel(quantity=2))
-  order.add(MockModel(quantity=2))
-  order.add(MockModel(quantity=2))
-  delivery_distance = 4
-  #Act
-  cost = Delivery.calculate(order,delivery_distance)
-  #Assert
-  assert cost == 5
+def test_lots_of_items():
+    order = MockSet()
+    order.add(MockModel(quantity=5))
+    order.add(MockModel(quantity=5))
+    order.add(MockModel(quantity=5))
 
-def test_LittleItems():
-  #Arrange
+    assert Delivery.calculate(order, distance=6) == 7.5
+
+
+def test_middle_of_the_road_items():
+    order = MockSet()
+    order.add(MockModel(quantity=2))
+    order.add(MockModel(quantity=2))
+    order.add(MockModel(quantity=2))
+
+    assert Delivery.calculate(order, distance=4) == 5
+
+
+def test_default_delivery_fee():
     order = MockSet()
     order.add(MockModel(quantity=3))
     order.add(MockModel(quantity=1))
-    delivery_distance = 2
-  #Act
-    cost = Delivery.calculate(order,delivery_distance)
-  #Assert
-    assert cost == 2.5
+
+    assert Delivery.calculate(order, distance=2) == 3.5
